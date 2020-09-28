f1573a3196ea58c212608ac7ff5adac3fca9a1dc
Florian Klimm <f.klimm@gmail.com>
2020-09-28 14:30:54 +0100

    added ER graphon function

---
+++ matlab/graphonConstruction/ERGraphon.m
@@ -0,0 +1,10 @@
+function [W] = ERGraphon(n,density)
+%Lambda graphon
+% Input: 
+%           n - number of `nodes' (discretisation)
+%           density - density parameter float in (0,1)
+
+W=density*ones(n); % set up the empty graphon
+
+end
+
